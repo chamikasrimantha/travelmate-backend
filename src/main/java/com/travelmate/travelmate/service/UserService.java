0095a91f59b5b079922c5a01d03cca8cb41cf58e
Chamika Srimantha <chamikasrimantha2002@gmail.com>
2024-06-20 13:43:44 +0530

    changes added

--- src/main/java/com/travelmate/travelmate/service/UserService.java
+++ src/main/java/com/travelmate/travelmate/service/UserService.java
@@ -1,28 +1,15 @@
 package com.travelmate.travelmate.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.travelmate.travelmate.dto.AdminDto;
 import com.travelmate.travelmate.dto.PartnerDto;
 import com.travelmate.travelmate.dto.UserDto;
-import com.travelmate.travelmate.dto.UserPwdDto;
 import com.travelmate.travelmate.entity.UserEntity;
 
 @Service
 public interface UserService {
     UserEntity createUser(UserDto userDto);
-
-    UserEntity createPartner(PartnerDto partnerDTO);
-    
-    UserEntity createAdmin(AdminDto adminDTO);
-
-    UserEntity updateUser(UserEntity userEntity);
-
-    UserEntity getUserById(Long id);
-
-    List<UserEntity> getAllUsers();
-
-    UserEntity changePassword(Long id, UserPwdDto userPwdDto);
+    UserEntity createPartner(PartnerDto partnerDtO);
+    UserEntity createAdmin(AdminDto adminDtO);
 }
--- src/main/java/com/travelmate/travelmate/service/impl/UserServiceImpl.java
+++ src/main/java/com/travelmate/travelmate/service/impl/UserServiceImpl.java
@@ -1,14 +1,13 @@
 package com.travelmate.travelmate.service.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.travelmate.travelmate.dto.AdminDto;
 import com.travelmate.travelmate.dto.PartnerDto;
 import com.travelmate.travelmate.dto.UserDto;
-import com.travelmate.travelmate.dto.UserPwdDto;
+import com.travelmate.travelmate.entity.Admin;
+import com.travelmate.travelmate.entity.Partner;
 import com.travelmate.travelmate.entity.User;
 import com.travelmate.travelmate.entity.UserEntity;
 import com.travelmate.travelmate.entity.UserRole;
@@ -20,12 +19,6 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Override
-    public UserEntity changePassword(Long id, UserPwdDto userPwdDto) {
-        // TODO Auto-generated method stub
-        return null;
-    }
 
     @Override
     public UserEntity createUser(UserDto userDto) {
@@ -42,34 +35,28 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserEntity createAdmin(AdminDto adminDTO) {
-        // TODO Auto-generated method stub
-        return null;
+    public UserEntity createAdmin(AdminDto adminDto) {
+        Admin admin = new Admin();
+        admin.setUsername(adminDto.getUsername());
+        admin.setEmail(adminDto.getEmail());
+        admin.setPassword(adminDto.getPassword());
+        admin.setRole(UserRole.ADMIN);
+        admin.setName(adminDto.getName());
+        return userRepository.save(admin);
     }
 
     @Override
-    public UserEntity createPartner(PartnerDto partnerDTO) {
-        // TODO Auto-generated method stub
-        return null;
+    public UserEntity createPartner(PartnerDto partnerDto) {
+        Partner partner = new Partner();
+        partner.setUsername(partnerDto.getUsername());
+        partner.setEmail(partnerDto.getEmail());
+        partner.setPassword(partnerDto.getPassword());
+        partner.setRole(UserRole.PARTNER);
+        partner.setFirstName(partnerDto.getFirstName());
+        partner.setLastName(partnerDto.getLastName());
+        partner.setPhoneNo(partnerDto.getPhoneNo());
+        partner.setAddress(partnerDto.getAddress());
+        return userRepository.save(partner);
     }
-
-    @Override
-    public List<UserEntity> getAllUsers() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public UserEntity getUserById(Long id) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public UserEntity updateUser(UserEntity userEntity) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
 
 }
